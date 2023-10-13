d47e3697791c9e38a2565bd10e77850e8c45e262
Marci <marton.antoni@gmail.com>
2023-10-13 18:16:46 +0200

    good enough

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# bolyai_pontszam
-Minimalistic score calculator for Bolyai competition
+﻿# Bolyai Pontszámoló
+
+Nagyon minimalista, stdin-ről olvas, stdout-ra ír.
+
+## Bemenet formátuma
+
+int: Hány darab feladat van
+feladat * string: helyes megoldás betűjelei 
+feladat * string: csapat által megadott válasz betűjelei, ha egyiket se jelölték, akkor egy - jel
+
+Mellékelve van egy példa bemeneti file, example_input.txt néven.
+
+## Kimenet
+
+Max score: hibátlan feladatsor ennyi pontot ér
+Score: ennyi pontot szerzett a csapat
---
+++ main.cpp
@@ -0,0 +1,149 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include <windows.h>
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <memory>
+#include <algorithm>
+#include <numeric>
+#include <string>
+#include <set>
+#include <tuple>
+#include <functional>
+#include <cstring>
+#include <deque>
+#include <array>
+#include <map>
+#include <random>
+#include <unordered_map>
+#include <unordered_set>
+#include <cstddef>
+#include <cinttypes>
+#include <regex>
+
+#undef max
+#undef min
+#undef I
+
+#ifdef __GNUC__ 
+#pragma GCC diagnostic ignored "-Wunused-result"
+#endif
+
+#ifdef _DEBUG
+#define ASSERT(x) if(!(x)) __debugbreak()
+#define LOG(...) fprintf(stderr, __VA_ARGS__)
+#define D(expression) expression
+#else
+char* crash_please = (char*)42;
+#define ASSERT(x) 
+//if(!(x)) { printf("%s failed",#x); *crash_please=33; }
+//#define LOG(...) fprintf(stderr, __VA_ARGS__)
+#define LOG(...)
+#define D(expression)
+#endif
+
+#include <chrono>
+
+class cLogPerformance_Guard
+{
+    std::chrono::time_point<std::chrono::high_resolution_clock> mStartTime = std::chrono::high_resolution_clock::now();
+    const char* mName;
+public:
+    cLogPerformance_Guard(const char* Name) : mName(Name) {}
+    ~cLogPerformance_Guard()
+    {
+        auto EndTime = std::chrono::high_resolution_clock::now();
+        auto Elapsed = std::chrono::duration_cast<std::chrono::microseconds>(EndTime - mStartTime);
+        LOG("\n--- Elapsed %llu.%llu ms in %s ---\n", (unsigned long long)Elapsed.count() / 1000, (unsigned long long)Elapsed.count() % 1000, mName);
+    }
+    void waypoint()
+    {
+        auto EndTime = std::chrono::high_resolution_clock::now();
+        auto Elapsed = std::chrono::duration_cast<std::chrono::microseconds>(EndTime - mStartTime);
+        LOG("+ %llu.%llu ms: ", (unsigned long long)Elapsed.count() / 1000, (unsigned long long)Elapsed.count() % 1000);
+    }
+} perf_guard("main");
+
+using namespace std;
+using namespace std::chrono;
+using namespace std::string_literals;
+using ull = unsigned long long;
+using ll = long long;
+#define RI(var_name) int var_name; scanf("%d", &var_name);
+#define RIV(var_name, size) vector<int> var_name(size); for(auto &item_of_##var_name: var_name) scanf("%d", &item_of_##var_name);
+#define RII(var_name1, var_name2) int var_name1, var_name2; scanf("%d %d", &var_name1, &var_name2);
+#define RIII(var_name1, var_name2, var_name3) int var_name1, var_name2, var_name3; scanf("%d %d %d", &var_name1, &var_name2, &var_name3);
+#define RIIII(var_name1, var_name2, var_name3, var_name4) int var_name1, var_name2, var_name3, var_name4; scanf("%d %d %d %d", &var_name1, &var_name2, &var_name3, &var_name4);
+#define RL(var_name) ll var_name; scanf("%lld", &var_name);
+#define RLV(var_name, size) vector<ll> var_name(size); for(auto &item_of_##var_name: var_name) scanf("%lld", &item_of_##var_name);
+#define RLUV(var_name, size) vector<ull> var_name(size); for(auto &item_of_##var_name: var_name) scanf("%llu", &item_of_##var_name);
+#define RLL(var_name1, var_name2) ll var_name1, var_name2; scanf("%lld %lld", &var_name1, &var_name2);
+#define RLLL(var_name1, var_name2, var_name3) ll var_name1, var_name2, var_name3; scanf("%lld %lld %lld", &var_name1, &var_name2, &var_name3);
+#define RLLLL(var_name1, var_name2, var_name3, var_name4) ll var_name1, var_name2, var_name3, var_name4; scanf("%lld %lld %lld %lld", &var_name1, &var_name2, &var_name3, &var_name4);
+#define RD(var_name) double var_name; scanf("%lf", &var_name);
+#define RDV(var_name, size) vector<double> var_name(size); for(auto &item_of_##var_name: var_name) scanf("%lf", &item_of_##var_name);
+#define RDD(var_name1, var_name2) double var_name1, var_name2; scanf("%lf %lf", &var_name1, &var_name2);
+#define RDDD(var_name1, var_name2, var_name3) double var_name1, var_name2, var_name3; scanf("%lf %lf %lf", &var_name1, &var_name2, &var_name3);
+#define ALL(cont) cont.begin(), cont.end()
+#define FOR(var, max_value) for(decltype(max_value) var=0;var<max_value;++var)
+
+constexpr ll mod = 1'000'000'007;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+void main()
+{
+    RI(n);
+    int score = 0, max_score=0;
+    vector<vector<bool>> official;
+    char l[6];
+    FOR(i, n)
+    {
+        scanf("%s", l);
+        vector<bool> p(5, false);
+        for (auto c : std::string(l))
+            p[tolower(c) - 'a'] = true;
+        official.emplace_back(std::move(p));
+    }
+    FOR(i, n)
+    {
+        scanf("%s", l);
+        vector<bool> p(5, false);
+        if (*l != '-')
+        {
+            for (auto c : std::string(l))
+                p[tolower(c) - 'a'] = true;
+        }
+        int all_good = true;
+        FOR(j, 5)
+        {
+            if (official[i][j])
+            {
+                max_score += 3;
+                if (p[j])
+                    score += 3;
+                else
+                {
+                    all_good = false;
+                }
+            }
+            else
+            {
+                max_score += 2;
+                if (p[j])
+                {
+                    score -= 1;
+                    all_good = false;
+                }
+                else
+                    score += 2;
+            }
+        }
+        max_score += 2;
+        if (all_good)
+            score += 2;
+    }
+    printf("Max score: %d\n", max_score);
+    printf("Score: %d\n", score);
+}
